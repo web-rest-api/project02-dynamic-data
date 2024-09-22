ec5bbe0c18e923f2aacbc60cb673d98876fa450a
lostmart <martinpedraza1979@gmail.com>
2024-09-22 19:38:47 +0200

    new fns

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 import "./css/bootstrap.min.css"
 import "./js/bootstrap.bundle.min"
+
+const URL = "https://jsonplaceholder.typicode.com/users"
+fetch(URL).then((res) => {
+	if (!res.ok) {
+		return (container.textContent = "no user found with this data")
+	}
+	res.json().then((json) => {
+		handleData(json)
+	})
+})
+
+function handleData(json) {
+	console.log(json)
+}
